00fbf925e907183a235345a67ed270f0283aa020
Frederico Muñoz <fsmunoz@gmail.com>
2025-09-27 19:40:33 +0100

    Add handler for /

--- tests/test-clack.lisp
+++ tests/test-clack.lisp
@@ -7,14 +7,6 @@
 
 (defparameter *clack-http-port* 7332 "Port in which to listen")
 
-(defun start-clack-server (&key (port *clack-http-port*))
-  "Start Clack server"
-  (setf *clack-handler* 
-        (clack:clackup 
-         (datastar-cl::make-clack-datastar-handler #'datastar-cl-tests::handle-datastar-signals )
-         :port port
-	 :server :woo))
-  (format t "Clack server started on port ~a~%" port))
 
 (defun stop-clack-server ()
   "Stop Clack server"
@@ -22,3 +14,28 @@
     (clack:stop *clack-handler*)
     (setf *clack-handler* nil)))
 
+(defun start-clack-server (&key (port *clack-http-port*))
+  "Start Clack server with routing."
+  (setf *clack-handler*
+        (clack:clackup
+         (lambda (env)
+           (let ((path (getf env :path-info)))
+             (cond
+               ;; Handle /test with your datastar handler
+               ((string= path "/test")
+                (funcall (datastar-cl::make-clack-datastar-handler
+                          #'datastar-cl-tests::handle-datastar-signals)
+                         env))
+
+               ;; Handle /
+               ((string= path "/")
+                '(200 (:content-type "text/plain")
+                      ("Hello! This is the root path.")))
+
+               ;; Default for other paths
+               (t
+                '(404 (:content-type "text/plain")
+                      ("Not Found"))))))
+         :port port
+         :server :woo))
+  (format t "Clack server started on port ~a~%" port))
